e97e4a96349bc3ab6bd839e321fe78552aefb723
pipeswk <afco1995@gmail.com>
2022-04-27 16:43:04 -0500

    Se agrega useRouter al logo del menu

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,10 +1,13 @@
 import Image from "next/image"
 import useQuiosco from "../hooks/useQuiosco"
 import Categoria from "./Categoria"
+import { useRouter } from 'next/router'
 
 const Sidebar = () => {
 
-    const { categorias } = useQuiosco()
+    const { categorias, handleChangePaso } = useQuiosco()
+
+    const router = useRouter()
 
 
   return (
@@ -14,6 +17,11 @@ const Sidebar = () => {
             height={100}
             src='/assets/img/logo.svg'
             alt='logotipo'
+            className='hover:cursor-pointer'
+            onClick={() => {
+                router.push('/')
+                handleChangePaso(1)
+            }}
         />
 
         <nav className='mt-10'>
