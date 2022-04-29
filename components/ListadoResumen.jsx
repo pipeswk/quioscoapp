de9a1d6fadc3455fa820f98f271e37440d0e1c74
pipeswk <afco1995@gmail.com>
2022-04-28 22:24:02 -0500

    Se agrega funcion para eliminar producto

--- components/ListadoResumen.jsx
+++ components/ListadoResumen.jsx
@@ -3,9 +3,9 @@ import useQuiosco from '../hooks/useQuiosco'
 
 const ListadoResumen = ( { producto } ) => {
 
-    const { handleEditarCantidades } = useQuiosco()
+    const { handleEditarCantidades, handleEliminarProducto } = useQuiosco()
 
-    const { nombre, precio, imagen, cantidad} = producto
+    const { nombre, precio, imagen, cantidad, id} = producto
 
     const moneda = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(precio)
     const subtotal = precio * cantidad
@@ -40,7 +40,12 @@ const ListadoResumen = ( { producto } ) => {
             </svg>
             Editar
           </button>
-          <button className='bg-red-700 px-3 py-1 rounded text-white font-bold hover:bg-red-800 shadow-md flex gap-1 w-full justify-center mb-4'>
+          <button
+            className='bg-red-700 px-3 py-1 rounded text-white font-bold hover:bg-red-800 shadow-md flex gap-1 w-full justify-center mb-4'
+            onClick={() => {
+              handleEliminarProducto(id)
+            }}
+          >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
             </svg>
